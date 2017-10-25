624ca3d81e59049568c5151b6ed50d2b177fc047
Henry Pan <henrypoon1231@gmail.com>
2017-10-26 09:42:58 +1100

    bucket sort version

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -5,6 +5,61 @@
 #include <thread>
 #include <memory>
 #include <mutex>
+#include <chrono>
+
+unsigned GetNumberOfDigits (unsigned int i)
+{
+    return i > 0 ? (int) log10 ((double) i) + 1 : 1;
+}
+
+signed GetIndex(unsigned int i, int bit) {
+    int numOfDigit = GetNumberOfDigits(i);
+    if (bit > numOfDigit - 1)
+        return -1;
+    else
+        return i / static_cast<int>(pow(10, numOfDigit - 1 - bit)) % 10;
+}
+
+std::vector<unsigned int> bucket_sort(std::vector<unsigned int> v, int left, int right, int bit)
+{
+    std::vector<unsigned int> result;
+    std::vector<unsigned int> bucket[11];
+    for(int i = left; i <= right; ++i) {
+        int idx = GetIndex(v[i], bit);
+        if (idx == -1)
+            bucket[0].push_back(v[i]);
+        else
+            bucket[idx + 1].push_back(v[i]);
+    }
+
+    if (bucket[0].size() > 0) {
+        std::move(std::begin(bucket[0]), std::end(bucket[0]), std::back_inserter(result));      
+    }
+
+    for(int i = 1; i < 11; ++i) {
+        if (bucket[i].size() > 0) {
+            // std::cout << i << " bucket_sort" << std::endl;
+            // exit(0);
+            
+            int temp = bucket[i][0];
+            bool allSame = true;
+            for (int j = 0; j < bucket[i].size(); ++j) {
+                if (bucket[i][j] != temp) {
+                    std::vector<unsigned int> vec = bucket_sort(bucket[i], 0, bucket[i].size() - 1, bit + 1);
+                    std::move(std::begin(vec), std::end(vec), std::back_inserter(result));      
+                    allSame = false;
+                    break;
+                }
+            }
+            if (allSame) {
+                std::move(std::begin(bucket[i]), std::end(bucket[i]), std::back_inserter(result));      
+            }
+        }
+        // exit(0);
+        bucket[i].clear();
+    }
+    return result;
+}
 
 
 bool aLessB(const unsigned int& x, const unsigned int& y, unsigned int pow) {    
@@ -42,7 +97,7 @@ unsigned GetBucketIndex(unsigned int i) {
 void BucketSort::sort(unsigned int numCores) {
     std::mutex addMutex;
     std::vector<std::shared_ptr<std::thread>> threads;
-    for (size_t i = 0; i != numCores; ++i) {
+    for (size_t i = 1; i != numCores; ++i) {
         threads.push_back(std::make_shared<std::thread>([this, &numCores, &addMutex](unsigned int id) {
             size_t idx;
             std::lock_guard<std::mutex> guard(addMutex);
@@ -62,16 +117,33 @@ void BucketSort::sort(unsigned int numCores) {
 
     int digits = 9;
     std::mutex sortMutex;
-    for (size_t i = 0; i != numCores; ++i) {
-        threads.push_back(std::make_shared<std::thread>([this, &sortMutex, &digits]() {
-            std::lock_guard<std::mutex> guard(sortMutex);
-            while(digits >= 0) {
-                int idx = digits;
-                // std::sort(bucket[idx].begin(), bucket[idx].end());
-                std::sort(bucket[idx].begin(),bucket[idx].end(), [](const unsigned int& x, const unsigned int& y){
-                    return aLessB(x,y,0);
-                });
-                --digits;
+    for (size_t i = 1; i != numCores; ++i) {
+        // std::cout << "sdfsfd " << i << std::endl;
+        threads.push_back(std::make_shared<std::thread>([this, &sortMutex, &digits, &i]() {
+            int idx;
+            while (true) {
+                std::lock_guard<std::mutex> guard(sortMutex);
+                {
+                    idx = digits;
+                    --digits;
+                }
+                if (idx < 0)
+                    break;
+                std::thread::id this_id = std::this_thread::get_id();
+                std::cout << "thread " << this_id << " is processing " << idx << " bucket" <<std::endl;
+
+                auto temp = bucket_sort(bucket[idx], 0, bucket[idx].size() - 1, 1);
+                bucket[idx].clear();
+                std::move(std::begin(temp), std::end(temp), std::back_inserter(bucket[idx]));
+                temp.clear();
+
+                // std::sort(bucket[idx].begin(),bucket[idx].end(), [](const unsigned int& x, const unsigned int& y){
+                //     return aLessB(x,y,0);
+                // });
+                // for (auto ite = bucket[2].begin(); ite != bucket[2].end(); ++ite) {
+                //     std::cout << *ite << " ";
+                // }
+                
             }
         }));
     }
@@ -84,15 +156,19 @@ void BucketSort::sort(unsigned int numCores) {
 
     digits = 0;
     std::mutex putBackMutex;
-    for (size_t i = 0; i != numCores; ++i) {
-        threads.push_back(std::make_shared<std::thread>([this, &putBackMutex, &digits]() {
-            std::lock_guard<std::mutex> guard(putBackMutex);
-            while(digits < 10) {
-                // std::cout << digits << std::endl;
-                int idx = digits;
+    for (size_t i = 1; i != numCores; ++i) {
+        threads.push_back(std::make_shared<std::thread>([this, &putBackMutex, &digits, &i]() {
+            int idx;
+            while(true) {
+                std::lock_guard<std::mutex> guard(putBackMutex);
+                {
+                    idx = digits;
+                    // bucket[idx].clear();
+                    ++digits;
+                }
+                if (idx > 9)
+                    break;
                 std::move(std::begin(bucket[idx]), std::end(bucket[idx]), std::back_inserter(numbersToSort));
-                // bucket[idx].clear();
-                ++digits;
             }
         }));
     }
@@ -105,9 +181,15 @@ void BucketSort::sort(unsigned int numCores) {
 
 // TODO: replace this with a parallel version. 
 void BucketSort::single_sort(unsigned int numCores) {
-        std::sort(numbersToSort.begin(),numbersToSort.end(), [](const unsigned int& x, const unsigned int& y){
-                return aLessB(x,y,0);
-        } );
+    // std::sort(numbersToSort.begin(),numbersToSort.end(), [](const unsigned int& x, const unsigned int& y){
+    //         return aLessB(x,y,0);
+    // } );
+    // for (auto ite = numbersToSort.begin(); ite != numbersToSort.end(); ++ite) {
+    //     std::cout << *ite << " ";
+    // }
+    auto test = bucket_sort(numbersToSort, 0, numbersToSort.size() - 1, 0);
+    numbersToSort.clear();
+    std::move(std::begin(test), std::end(test), std::back_inserter(numbersToSort));
 }
 
 
--- sortTester.cpp
+++ sortTester.cpp
@@ -1,13 +1,83 @@
 #include <iostream>
 #include <random>
 #include <thread>
+#include <cstring>
 #include <string>
 #include <fstream>
 
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <cstdlib>
+
 #include "BucketSort.h"
+
+// std::vector<unsigned int> test = {48, 4, 2,2,454,2,1,10,24,1021,12,154};
+
+
+// unsigned GetNumberOfDigits (unsigned i)
+// {
+//     return i > 0 ? (int) log10 ((double) i) + 1 : 1;
+// }
+
+// signed GetIndex(unsigned int i, int bit) {
+// 	int numOfDigit = GetNumberOfDigits(i);
+// 	if (bit > numOfDigit - 1)
+// 		return -1;
+// 	else
+// 	    return i / static_cast<int>(pow(10, numOfDigit - 1 - bit)) % 10;
+// }
+
+// std::vector<unsigned int> sort(std::vector<unsigned int> v, int left, int right, int bit)
+// {
+
+//     std::vector<unsigned int> result;
+//     std::vector<unsigned int> bucket[11];
+//     for(int i = left; i <= right; ++i) {
+//         int idx = GetIndex(v[i], bit);
+//         if (idx == -1)
+//             bucket[0].push_back(v[i]);
+//         else
+//             bucket[idx + 1].push_back(v[i]);
+//     }
+
+//     if (bucket[0].size() > 0) {
+//         std::move(std::begin(bucket[0]), std::end(bucket[0]), std::back_inserter(result));      
+//     }
+
+//     for(int i = 1; i < 11; ++i) {
+//         if (bucket[i].size() > 0) {
+//             // exit(0);
+            
+//             int temp = bucket[i][0];
+//             bool allSame = true;
+//             for (int j = 0; j < bucket[i].size(); ++j) {
+//                 if (bucket[i][j] != temp) {
+//                     std::vector<unsigned int> vec = sort(bucket[i], 0, bucket[i].size() - 1, bit + 1);
+//                     std::move(std::begin(vec), std::end(vec), std::back_inserter(result));      
+//                    	allSame = false;
+//                     break;
+//                 }
+//             }
+//             if (allSame) {
+//             	std::move(std::begin(bucket[i]), std::end(bucket[i]), std::back_inserter(result));      
+//             }
+//         }
+//         // exit(0);
+//         bucket[i].clear();
+//     }
+//     return result;
+// }
+
 
 int main() {
 	
+	// auto v = sort(test, 0, test.size() - 1, 0);
+
+	// for (auto ite = v.begin(); ite != v.end(); ++ite) {
+	// 	std::cout << *ite << " " << std::endl;
+	// }
+
 	unsigned int totalNumbers =	500000;
 	unsigned int printIndex =	259000;
 	
@@ -18,10 +88,10 @@ int main() {
 
 	// create a sort object
 	BucketSort pbs;
-	BucketSort single_test;
+	// BucketSort single_test;
 
-	std::ofstream ofs1("out1");
-	std::ofstream ofs2("out2");
+	// std::ofstream ofs1("out1");
+	// std::ofstream ofs2("out2");
 
 	// insert random numbers into the sort object
 	for (unsigned int i=0; i < totalNumbers; ++i) {
@@ -29,17 +99,17 @@ int main() {
 	} 
 
 
-	for (size_t i = 0; i < totalNumbers; ++i) {
-		single_test.numbersToSort.push_back(pbs.numbersToSort.at(i));
-	}
+	// for (size_t i = 0; i < totalNumbers; ++i) {
+		// single_test.numbersToSort.push_back(pbs.numbersToSort.at(i));
+	// }
 
 	// call sort giving the number of cores available.
     const unsigned int numCores = std::thread::hardware_concurrency();
     pbs.sort(numCores);
-    single_test.single_sort(numCores);
-    
-	std::copy(pbs.numbersToSort.begin(), pbs.numbersToSort.end(), std::ostream_iterator<unsigned int>(ofs1, " "));
-	std::copy(single_test.numbersToSort.begin(), single_test.numbersToSort.end(), std::ostream_iterator<unsigned int>(ofs2, " "));
+    // single_test.single_sort(numCores);
+
+	// std::copy(pbs.numbersToSort.begin(), pbs.numbersToSort.end(), std::ostream_iterator<unsigned int>(ofs1, " "));
+	// std::copy(single_test.numbersToSort.begin(), single_test.numbersToSort.end(), std::ostream_iterator<unsigned int>(ofs2, " "));
 
     std::cout << "number of cores used: " << numCores << std::endl;
 	
@@ -49,29 +119,34 @@ int main() {
 		<< " " << pbs.numbersToSort[printIndex] << " " << pbs.numbersToSort[pbs.numbersToSort.size() - 1] 
 		<< std::endl;
 	
-	std::cout << "Demonstrating that all the numbers that start with 1 come first" << std::endl;
-	std::cout << single_test.numbersToSort[0] << " " << single_test.numbersToSort[printIndex - 10000]
-		<< " " << single_test.numbersToSort[printIndex] << " " << single_test.numbersToSort[single_test.numbersToSort.size() - 1] 
-		<< std::endl;
+
+
+
+
+
+	// std::cout << "Demonstrating that all the numbers that start with 1 come first" << std::endl;
+	// std::cout << single_test.numbersToSort[0] << " " << single_test.numbersToSort[printIndex - 10000]
+	// 	<< " " << single_test.numbersToSort[printIndex] << " " << single_test.numbersToSort[single_test.numbersToSort.size() - 1] 
+	// 	<< std::endl;
 
 
 	// BucketSort b;
-	// b.numbersToSort.push_back(0);
-	// b.numbersToSort.push_back(100);
+	// // b.numbersToSort.push_back(0);
+	// // b.numbersToSort.push_back(100);
 	// b.numbersToSort.push_back(3);
-	// b.numbersToSort.push_back(65);
-	// b.numbersToSort.push_back(2121);
-	// b.numbersToSort.push_back(21);
+	// // b.numbersToSort.push_back(65);
+	// b.numbersToSort.push_back(25);
+	// b.numbersToSort.push_back(2321);
 	// b.numbersToSort.push_back(2);
-	// b.numbersToSort.push_back(254654);
+	// // b.numbersToSort.push_back(254654);
 	// b.numbersToSort.push_back(8);
 	// b.numbersToSort.push_back(888);
 	// b.numbersToSort.push_back(4545);
-	// b.numbersToSort.push_back(9);
-	// b.numbersToSort.push_back(95);
+	// // b.numbersToSort.push_back(9);
+	// // b.numbersToSort.push_back(95);
 
 
-	// b.single_sort(4);
+	// b.sort(4);
 
 	// for (size_t i = 0; i < 10; ++i) {
 	// 	std::cout << i << " bucket has: ";
@@ -88,4 +163,15 @@ int main() {
 	// 	std::cout << *ite << " ";
 	// }
 	// std::cout << std::endl;
+	// int data[] = { 170, 45, 75, -90, -802, 24, 2, 66 };
+	// std::vector<int> v = {1, 2};
+
+	// sort(v.begin(), v.end());
+
+	// for (auto ite = v.begin(); ite != v.end(); ++ite) {
+	// 	std::cout << *ite << std::endl;
+		
+	// }
+	
+
 }
